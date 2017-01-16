bdad144c6ba64130d12e94fd3ab295d5adbe4991
VaTz88 <vatz88@gmail.com>
2017-01-16 16:01:14 +0530

    bot route added

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var bot = require('./routes/bot');
 
 var app = express();
+
+// https redirect
+// app.get("*", function (req, res, next) {
+//     if (!req.secure) {
+//         return res.redirect('https://' + req.headers.host + req.url);
+//     }
+//     next();
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +32,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/bot', bot);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -36,7 +46,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -47,7 +57,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
